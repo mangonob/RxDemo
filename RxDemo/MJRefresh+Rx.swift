4dee39da2424af43f152c52cf00877b50ee0a6b4
mangonob <534393806@qq.com>
2019-04-04 14:47:36 +0800

    - Pagenation

--- RxDemo/MJRefresh+Rx.swift
+++ RxDemo/MJRefresh+Rx.swift
@@ -15,4 +15,24 @@ extension Reactive where Base: MJRefreshComponent {
     var refreshing: ControlEvent<Void> {
         return ControlEvent(events: base.rx.methodInvoked(#selector(base.beginRefreshing as () -> Void)).map { _ -> Void in })
     }
+    
+    var endRefreshing: Binder<Void> {
+        return Binder.init(base, binding: { (target, _) in
+            target.endRefreshing()
+        })
+    }
+}
+
+extension Reactive where Base: MJRefreshFooter {
+    var endWithNoMoreData: Binder<Void> {
+        return Binder.init(base, binding: { (target, _) in
+            target.endRefreshingWithNoMoreData()
+        })
+    }
+    
+    var resetNoMoreData: Binder<Void> {
+        return Binder.init(base, binding: { (target, _) in
+            target.resetNoMoreData()
+        })
+    }
 }
--- RxDemo/TableViewController.swift
+++ RxDemo/TableViewController.swift
@@ -16,106 +16,108 @@ enum RuntimeError: Error {
     case loadError(String?)
 }
 
-class ScrollViewPagenation<Element>: Pagenation<Element> {
-    internal (set) weak var scrollView: UIScrollView!
-    
-    override var state: PagenationState {
-        didSet {
-            switch state {
-            case is PagenationStateError:
-                refreshHeader().endRefreshing()
-                refreshFooter().endRefreshing()
-            case is PagenationStateFetchedContents<Element>:
-                refreshHeader().endRefreshing()
-                refreshFooter().endRefreshing()
-            case is PagenationStateCompleted:
-                refreshHeader().endRefreshing()
-                refreshFooter().endRefreshingWithNoMoreData()
-            default: break
-            }
-        }
-    }
-    
-    func refreshHeader() -> MJRefreshHeader {
-        return scrollView.mj_header
-    }
-    
-    func refreshFooter() -> MJRefreshFooter {
-        return scrollView.mj_footer
-    }
-    
-    override func reloadData(_ loader: (@escaping (Error?, [Element]?) -> Void) -> Void) {
-        if state is PagenationStateLoading {
-            scrollView.mj_header.endRefreshing()
-        }
-        
-        super.reloadData(loader)
-    }
-    
-    override func loadMoreData(_ loader: (@escaping (Error?, [Element]?) -> Void) -> Void) {
-        if state is PagenationStateLoading {
-            scrollView.mj_footer.endRefreshing()
-        }
-        
-        super.loadMoreData(loader)
-    }
+fileprivate enum CCPagenatorState<T> {
+    case initial
+    case completed
+    case fetchContents([T])
+    case error
+}
 
-    init(_ scrollView: UIScrollView) {
-        self.scrollView = scrollView
+class CCPagenator<T: Codable> {
+    var isLoading: Driver<Bool>
+    var contents = BehaviorSubject<[T]>(value: [])
+    var provider = CCPagenatorProvider<T>()
+
+    var endRefreshing: Driver<Void>!
+    var endLoadMore: Driver<Void>!
+    var endNoMoreData: Driver<Void>!
+    var resetNoMoreData: Driver<Void>!
+
+    private var disposeBag = DisposeBag()
+
+    init(input: (refreshing: Observable<Void>, loadMore: Observable<Void>)) {
+        let activity = RXActivity()
+        isLoading = activity.asDriver(onErrorJustReturn: false)
+        
+        let refreshingState = input.refreshing.share().withLatestFrom(activity).filter { !$0 }
+            .flatMapLatest { (_) -> Observable<[T]> in
+                return self.observableForRefreshing().trackActivity(by: activity)
+            }.map { (value) -> CCPagenatorState<T> in
+                return value.isEmpty ? .completed : .fetchContents(value)
+            }.flatMap { (state) -> Observable<CCPagenatorState<T>> in
+                switch state {
+                case .completed, .fetchContents(_):
+                    return Observable.of(CCPagenatorState.initial, state)
+                default:
+                    return Observable.just(state)
+                }
+            }.catchErrorJustReturn(.error)
+        
+        let loadMoreState = input.loadMore.share().withLatestFrom(activity).filter { !$0 }
+            .flatMapLatest { (_) -> Observable<[T]> in
+                return self.observableForLoadMore().trackActivity(by: activity)
+            }.map { (value) -> CCPagenatorState<T> in
+                return value.isEmpty ? .completed : .fetchContents(value)
+            }.catchErrorJustReturn(.error)
+        
+        let status = Observable<CCPagenatorState<T>>.merge([refreshingState, loadMoreState]).share()
+        
+        status.scan([T]()) { (contents, state) -> [T] in
+            switch state {
+            case .initial:
+                return []
+            case .fetchContents(let values):
+                return contents + values
+            default:
+                return contents
+            }
+            }.bind(to: contents).disposed(by: disposeBag)
+        
+        let endRefresh = status.filter { (state) -> Bool in
+            switch state {
+            case .completed, .fetchContents(_), .error:
+                return true
+            default:
+                return false
+            }
+            }.map{ _ -> Void in }
+
+        endRefreshing = Observable.merge([
+            endRefresh,
+            input.refreshing.share().withLatestFrom(activity).filter { $0 }.map { _ -> Void in }
+            ]).asDriver(onErrorJustReturn: ())
+        
+        endLoadMore = Observable.merge([
+            endRefresh,
+            input.loadMore.share().withLatestFrom(activity).filter { $0 }.map { _ -> Void in }
+            ]).asDriver(onErrorJustReturn: ())
+    }
+    
+    func observableForRefreshing() -> Observable<[T]> {
+        return provider.observableForRefresh(self)
+    }
+    
+    func observableForLoadMore() -> Observable<[T]> {
+        return provider.observableForLoadMore(self)
     }
 }
 
-class TableViewPagenation<Element>: ScrollViewPagenation<Element> {
-    override var state: PagenationState {
-        didSet {
-            switch state {
-            case is PagenationStateFetchedContents<Element>:
-                tableView.reloadData()
-            case is PagenationStateCompleted:
-                tableView.reloadData()
-            default: break
-            }
-        }
-    }
-    
-    var tableView: UITableView! {
-        set {
-            scrollView = newValue
-        }
-        get {
-            return (scrollView as! UITableView)
-        }
-    }
-    
-    init(_ tableView: UITableView) {
-        super.init(tableView)
-    }
-}
+class CCPagenatorProvider<T: Codable> {
+    typealias Provider = (CCPagenator<T>) -> Observable<[T]>
+    private var refreshingProvider: Provider?
+    private var loadMoreProvider: Provider?
 
-class CollectionViewPagenation<Element>: ScrollViewPagenation<Element> {
-    override var state: PagenationState {
-        didSet {
-            switch state {
-            case is PagenationStateFetchedContents<Element>:
-                collectionView.reloadData()
-            case is PagenationStateCompleted:
-                collectionView.reloadData()
-            default: break
-            }
-        }
+    func observableForRefresh(_ pagenator: CCPagenator<T>) -> Observable<[T]> {
+        return refreshingProvider?(pagenator) ?? Observable.empty()
     }
     
-    var collectionView: UICollectionView! {
-        set {
-            scrollView = newValue
-        }
-        get {
-            return (scrollView as! UICollectionView)
-        }
+    func observableForLoadMore(_ pagenator: CCPagenator<T>) -> Observable<[T]> {
+        return loadMoreProvider?(pagenator) ?? Observable.empty()
     }
     
-    init(_ collectionView: UICollectionView) {
-        super.init(collectionView)
+    init(refreshing: Provider? = nil, loadMore: Provider? = nil) {
+        self.refreshingProvider = refreshing
+        self.loadMoreProvider = loadMore
     }
 }
 
@@ -123,42 +125,40 @@ class TableViewController: UITableViewController {
     @IBOutlet weak var errorLabel: UITextField!
     @IBOutlet weak var contentsLabel: UITextField!
     
-    fileprivate lazy var pagenation = TableViewPagenation<Int>.init(self.tableView)
+    var pagenator: CCPagenator<Int>!
     private var disposeBag = DisposeBag()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.mj_header = MJRefreshNormalHeader(refreshingTarget: nil, refreshingAction: nil)
         tableView.mj_footer = MJRefreshAutoNormalFooter(refreshingTarget: nil, refreshingAction: nil)
-
-        tableView.mj_header.rx.refreshing.bind(to: pagenation.rx.reloadData(loader: { [weak self] (completion) in
-            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(800), execute: {
-                if let errorDescription = self?.errorLabel.text,
-                    !errorDescription.isEmpty {
-                    completion(RuntimeError.loadError(errorDescription), nil)
-                    return
-                }
-                
-                let contentsDescription = self?.contentsLabel.text
-                let contents = contentsDescription?.split(separator: ".").compactMap { Int($0) } ?? []
-                completion(nil, contents)
-            })
-        })).disposed(by: disposeBag)
         
-        tableView.mj_footer.rx.refreshing.bind(to: pagenation.rx.loadMoreData(loader: { [weak self] (completion) in
-            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(800), execute: {
-                if let errorDescription = self?.errorLabel.text,
-                    !errorDescription.isEmpty {
-                    completion(RuntimeError.loadError(errorDescription), nil)
-                    return
-                }
+        let headerRefreshing = tableView.mj_header.rx.refreshing.asObservable()
+        let footerRefreshing = tableView.mj_footer.rx.refreshing.asObservable()
+        
+        pagenator = CCPagenator<Int>(input: (headerRefreshing, footerRefreshing))
+        pagenator.endRefreshing.drive(tableView.mj_header.rx.endRefreshing).disposed(by: disposeBag)
+        pagenator.endLoadMore.drive(tableView.mj_footer.rx.endRefreshing).disposed(by: disposeBag)
+        pagenator.provider = CCPagenatorProvider<Int>.init(refreshing: { (_) -> Observable<[Int]> in
+            return Observable<[Int]>.create({ (observer) -> Disposable in
+                DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(1000), execute: {
+                    observer.onNext([1])
+                    observer.onCompleted()
+                })
                 
-                let contentsDescription = self?.contentsLabel.text
-                let contents = contentsDescription?.split(separator: ".").compactMap { Int($0) } ?? []
-                completion(nil, contents)
+                return Disposables.create()
             })
-        })).disposed(by: disposeBag)
+        }, loadMore: { (_) -> Observable<[Int]> in
+            return Observable<[Int]>.create({ (observer) -> Disposable in
+                DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(1000), execute: {
+                    observer.onNext([1])
+                    observer.onCompleted()
+                })
+                
+                return Disposables.create()
+            })
+        })
     }
     
     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
@@ -174,12 +174,12 @@ class TableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return pagenation.contents.count
+        return 0
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.detailTextLabel?.text = pagenation.contents[indexPath.row].description
+//        cell.detailTextLabel?.text = pagenation.contents[indexPath.row].description
         cell.textLabel?.text = indexPath.row.description
         return cell
     }
